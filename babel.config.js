d3078ce3609680629d6d13e65cf1146d28804d4f
Park <pjane123@naver.com>
2021-10-30 22:24:06 +0900

    chore: add babel-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['babel-plugin-styled-components']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "module-resolver",
+      {
+				root: ['./src'],
+				extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+				alias: {
+					'@': './src',
+					'@utils': './src/utils',
+					'@assets': './src/assets',
+					'@components': './src/components',
+					'@styles': './src/styles',
+				},
+			},
+    ],
   };
 };
