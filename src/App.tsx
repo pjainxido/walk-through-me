064a42018ad391ba461d78b33d8077f7e1e70a55
Park <pjane123@naver.com>
2021-11-04 23:48:38 +0900

    chore: async-storage 패키지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import { Appearance, ColorSchemeName } from "react-native";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import "react-native-gesture-handler";
-import styled, { ThemeProvider } from "styled-components/native";
-import { light, dark } from "@styles/theme";
-import Stack from "./navigation/Stack";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { Appearance, ColorSchemeName } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import styled, { ThemeProvider } from 'styled-components/native';
+import { light, dark } from '@styles/theme';
+import Stack from './navigation/Stack';
 
 export const App = () => {
   const [theme, setTheme] = useState<ColorSchemeName>(null);
   useEffect(() => {
     Appearance.addChangeListener(({ colorScheme }) => {
-      setTheme(colorScheme === "dark" ? "dark" : "light");
+      setTheme(colorScheme === 'dark' ? 'dark' : 'light');
     });
     return () => {};
   }, []);
 
   return (
-    <ThemeProvider theme={theme === "light" ? light : dark}>
+    <ThemeProvider theme={theme === 'light' ? light : dark}>
       <NavigationContainer
         theme={{
           ...DefaultTheme,
           colors: {
             ...DefaultTheme.colors,
-            background: theme === "light" ? light.mainBackground : dark.mainBackground,
-          },
+            background:
+              theme === 'light' ? light.mainBackground : dark.mainBackground
+          }
         }}
       >
         <Stack />
