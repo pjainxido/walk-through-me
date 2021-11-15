663d5199d90a709d5198f0ebd5280f2b9debf131
Park <pjane123@naver.com>
2021-11-15 16:39:59 +0900

    style: Home Screen 아이콘 추가

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -1,23 +1,28 @@
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
-import styled from "styled-components/native";
-import { StackNavigationProp } from "@react-navigation/stack";
+import React, { useContext } from 'react';
+import { Text } from 'react-native';
+import styled from 'styled-components/native';
+import { ThemeContext } from 'styled-components/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import ScreenLayout from '@/components/common/ScreenLayout';
-import { useNavigation } from "@react-navigation/native";
-import { RootStackParamList } from "./RootStackParams";
-import {NavButton} from '@/components/common'
+import { useNavigation } from '@react-navigation/native';
+import { RootStackParamList } from './RootStackParams';
+import { MaterialIcons } from '@expo/vector-icons';
+import { NavButton } from '@/components/common';
 
-type homeScreenProp = StackNavigationProp<RootStackParamList, "Home">;
+type homeScreenProp = StackNavigationProp<RootStackParamList, 'Home'>;
 
 const Home = () => {
+  const themeContext = useContext(ThemeContext);
   const navigation = useNavigation<homeScreenProp>();
+  //timer, log
+
   return (
     <ScreenLayout>
-      <NavButton onPress={() => navigation.navigate("Timer")}>
-        <Text>Timer</Text>
+      <NavButton onPress={() => navigation.navigate('Timer')}>
+        <MaterialIcons name="timer" size={24} color={themeContext.primaryText} />
       </NavButton>
-      <NavButton onPress={() => navigation.navigate("Log")}>
-        <Text>Log</Text>
+      <NavButton onPress={() => navigation.navigate('Log')}>
+        <MaterialIcons name="developer-board" size={24} color={themeContext.primaryText} />
       </NavButton>
     </ScreenLayout>
   );
