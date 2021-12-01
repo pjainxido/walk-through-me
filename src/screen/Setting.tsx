ac2a3e3192928071ac76032d55b99956f79c10e4
Park <pjane123@naver.com>
2021-12-01 22:36:06 +0900

    feat: add react-native Picker

--- src/screen/Setting.tsx
+++ src/screen/Setting.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/native';
+import { Picker } from '@react-native-picker/picker';
 
 import { View, Switch, SafeAreaView } from 'react-native';
 import { getItemFromAsync, setItemToAsync } from '@utils/common';
@@ -9,7 +10,20 @@ import useTheme from '@/utils/hooks/useTheme';
 const Setting = () => {
   const { primaryText, subColor, mainBackground, subBackground } = useTheme();
   const [menuRight, setMenuRight] = useState<boolean>(false);
+  const [selectedTheme, setSelectedTheme] = useState<string>('');
   const [iconTextPrint, setIconTextPrint] = useState<boolean>(false);
+
+  const themeOptions = [
+    {
+      value: 'Banana'
+    },
+    {
+      value: 'Mango'
+    },
+    {
+      value: 'Pear'
+    }
+  ];
   const toggleMenu = () => {
     setMenuRight((prev) => !prev);
   };
@@ -25,6 +39,23 @@ const Setting = () => {
       <Container>
         <OptionContainer>
           <InfoText>Theme</InfoText>
+          <Picker
+            style={{
+              height: 100,
+              width: 200,
+              position: 'relative',
+              zIndex: 5000,
+            }}
+            itemStyle={{
+              color: primaryText
+            }}
+            selectedValue={selectedTheme}
+            onValueChange={(itemValue) => setSelectedTheme(itemValue)}
+          >
+            <Picker.Item label="device theme" value="" />
+            <Picker.Item label="light" value="light" />
+            <Picker.Item label="dark" value="dark" />
+          </Picker>
         </OptionContainer>
         <OptionContainer>
           <InfoText>Menu Position</InfoText>
@@ -58,6 +89,17 @@ const Setting = () => {
   );
 };
 
+const PickerContainer = styled.View`
+  align-self: center;
+  /* width: 314;
+  border-width: 1;
+  border-color: 'rgba(155,155,155,1)';
+  border-bottom-left-radius: 10;
+  background-color: 'rgba(214,210,210,1)';
+  margin-top: 10;
+  margin-left: 4; */
+`;
+
 const Container = styled.View`
   margin: 50px;
   flex: 1;
@@ -66,7 +108,7 @@ const Container = styled.View`
   justify-content: space-evenly;
 `;
 const OptionContainer = styled.View`
-  flex: 1;
+  flex: 2;
   width: 100%;
   margin-bottom: 50px;
 `;
