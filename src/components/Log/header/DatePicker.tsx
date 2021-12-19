6207be5b7bc04f7abd9347e5ad2b9f14b51b1feb
Park <pjane123@naver.com>
2021-12-19 21:01:24 +0900

    feat: add DatePicker Options

--- src/components/Log/header/DatePicker.tsx
+++ src/components/Log/header/DatePicker.tsx
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/native';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { View } from 'react-native';
 import { CalendarIcon } from '@/components/common/icons';
+import { DropDownArrowIcon, CheckIcon } from '@/components/common/icons';
 import useTheme from '@/utils/hooks/useTheme';
 
+type DatePickerDropDown = 'day' | 'week' | '1month' | '3month' | '1year';
+
 const DatePicker = () => {
-  const { primaryText } = useTheme();
+  const { primaryText, subBackground } = useTheme();
+  const [openDropDown, setOpenDropDown] = useState(false);
+  const [dropDownValue, setDropdownValue] =
+    useState<DatePickerDropDown>('week');
+  const dropDownItems = [
+    { label: '1 day', value: 'day' },
+    { label: '1 week', value: 'week' },
+    { label: '1 month', value: '1month' },
+    { label: '3 month', value: '3month' },
+    { label: '1 year', value: '1year' }
+  ];
   return (
     <Container>
       <DatePickerContainer>
         <CalendarIcon size={15} defaultColor={primaryText} />
+        <DropDownPicker
+          open={openDropDown}
+          setOpen={setOpenDropDown}
+          value={dropDownValue ? dropDownValue.toString() : 'default'}
+          setValue={setDropdownValue}
+          items={dropDownItems}
+          ArrowDownIconComponent={() => (
+            <DropDownArrowIcon
+              size={24}
+              isDown={true}
+              defaultColor={primaryText}
+            />
+          )}
+          ArrowUpIconComponent={() => (
+            <DropDownArrowIcon size={24} defaultColor={primaryText} />
+          )}
+          TickIconComponent={() => (
+            <CheckIcon size={24} defaultColor={primaryText} />
+          )}
+          style={{
+            backgroundColor: subBackground,
+            borderStyle: 'dotted'
+          }}
+          dropDownContainerStyle={{
+            backgroundColor: subBackground
+          }}
+          textStyle={{
+            color: primaryText
+          }}
+        />
       </DatePickerContainer>
     </Container>
   );
